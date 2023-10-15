52919935c94b80b7b75add17ed5f9b1238938f05
vishnu biju <vishnubiju.in@gmail.com>
2023-10-15 20:13:43 +0530

    f8

--- src/components/BannerTwo.jsx
+++ src/components/BannerTwo.jsx
@@ -9,24 +9,9 @@ const BannerTwo = () => {
         alt="Banner01"
         className="w-full object-cover"
       />
-      <button
-        className="absolute top-3/4 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-10 font-semibold border-2 border-black py-1 px-2 sm:py-0.5 sm:px-1 sm:right-4 text-xxs sm:text-xs"
-        onMouseOver={(e) => {
-          e.target.style.backgroundColor = "black";
-          e.target.style.color = "white";
-        }}
-        onMouseOut={(e) => {
-          e.target.style.backgroundColor = "transparent";
-          e.target.style.color = "black";
-        }}
-      >
-        SHOP NOW
-      </button>
+      
     </div>
   );
 };
 
 export default BannerTwo;
-
-
-
